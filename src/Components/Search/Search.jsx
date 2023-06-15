abe5b146d1352d9feefb5baef3fbb8fa77506333
davidBilson <davidbilson99@gmail.com>
2023-06-15 14:42:00 +0100

    added search function to the input - can now fetch data from local JSON, render it in the search filter

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,9 +1,53 @@
-const Search = () => {
-  return (
-      <div>
-        <input type="text" placeholder='Search AI tool...' />
-      </div>
-  )
-}
+import { useEffect, useState } from 'react';
+import AIData from '../AIToolsData/AIData.json';
+import './Search.css';
 
-export default Search
+const Search = () => {
+  useEffect(() => {
+
+    const data = AIData
+    console.log(data)
+    // setData(data);
+    setFilterData(data);
+
+  }, []);
+
+  const [data, setData] = useState([]);
+
+  const [filterData, setFilterData] = useState([]);
+
+  const handleFilter = (value) => {
+  const result = filterData.filter(f => f.name.toLowerCase().includes(value.toLowerCase()));
+  if (value === "") {
+    setData(AIData);
+  } else {
+    setData(result);
+  }
+};
+
+
+  return (
+    <div className="searchTop">
+      <div>
+        <input type="text" onChange={e => handleFilter(e.target.value)} placeholder="Search AI tool..." />
+      </div>
+      <div className='searchResult'>
+        {data.map((d, i) => {
+              return (
+                <div key={i}>
+                  {d.name}
+                </div>
+              );
+            }
+          )
+        }
+      </div>
+      <div className="displayComponent">
+        
+      </div>
+    </div>
+    
+  );
+};
+
+export default Search;
