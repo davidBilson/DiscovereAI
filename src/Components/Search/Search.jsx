f3a8168bf8b85c5505518d4e6099acd474ee47ee
davidBilson <davidbilson99@gmail.com>
2023-06-16 06:01:27 +0100

    feat: added display component to search results

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -3,47 +3,72 @@ import AIData from '../AIToolsData/AIData.json';
 import './Search.css';
 
 const Search = () => {
-  useEffect(() => {
 
+  useEffect(() => {
     const data = AIData
     console.log(data)
-    // setData(data);
-    setFilterData(data);
+    setFilterData(data)
+  }, [])
 
-  }, []);
-
-  const [data, setData] = useState([]);
-
-  const [filterData, setFilterData] = useState([]);
+  const [data, setData] = useState([])
+  const [filterData, setFilterData] = useState([])
+  const [selectedTool, setSelectedTool] = useState([])
+  const [styleSearchResult, setStyleSearchResult] = useState({
+    display: "none"
+  })
 
   const handleFilter = (value) => {
   const result = filterData.filter(f => f.name.toLowerCase().includes(value.toLowerCase()));
   if (value === "") {
-    setData(AIData);
+    setData(AIData)
   } else {
-    setData(result);
+    setData(result)
   }
-};
+}
 
+const showSearchResultBar = () => {
+  setStyleSearchResult(prev => {
+    return {
+      ...prev,
+      display: "flex"
+    }
+  })
+}
+
+const hideSearchResultBar = () => {
+  setStyleSearchResult(prev => {
+    return {
+      ...prev,
+      display: "none"
+    }
+  })
+}
+
+const handleToolClick = (tool) => {
+  setSelectedTool(tool)
+}
 
   return (
     <div className="searchTop">
       <div>
-        <input type="text" onChange={e => handleFilter(e.target.value)} placeholder="Search AI tool..." />
+        <input type="text" onBlur={hideSearchResultBar}  onKeyDown={showSearchResultBar} onChange={e => handleFilter(e.target.value)} placeholder="Search AI tool..." />
       </div>
-      <div className='searchResult'>
+      <div className='searchResult' style={styleSearchResult}>
         {data.map((d, i) => {
               return (
-                <div key={i}>
-                  {d.name}
-                </div>
+                <div className='seachResult' key={i} onClick={() => handleToolClick(d)}> {d.name} </div>
               );
             }
           )
         }
       </div>
       <div className="displayComponent">
-        
+        { selectedTool && (
+          <div>
+              <h2>{selectedTool.name}</h2>
+              <h2>{selectedTool.generate}</h2>
+          </div>
+        )}
       </div>
     </div>
     
