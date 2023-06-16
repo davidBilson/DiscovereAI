a75c50901cb6ab5e8f4b11d0d62a43862b0c1b45
davidBilson <davidbilson99@gmail.com>
2023-06-16 06:23:43 +0100

    fixed: display content and search result

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -3,75 +3,55 @@ import AIData from '../AIToolsData/AIData.json';
 import './Search.css';
 
 const Search = () => {
-
   useEffect(() => {
-    const data = AIData
-    console.log(data)
-    setFilterData(data)
-  }, [])
+    setFilterData(AIData);
+  }, []);
 
-  const [data, setData] = useState([])
-  const [filterData, setFilterData] = useState([])
-  const [selectedTool, setSelectedTool] = useState([])
-  const [styleSearchResult, setStyleSearchResult] = useState({
-    display: "none"
-  })
+  const [data, setData] = useState(AIData);
+  const [filterData, setFilterData] = useState(AIData);
+  const [selectedTool, setSelectedTool] = useState(null);
 
   const handleFilter = (value) => {
-  const result = filterData.filter(f => f.name.toLowerCase().includes(value.toLowerCase()));
-  if (value === "") {
-    setData(AIData)
-  } else {
-    setData(result)
-  }
-}
+    const result = filterData.filter((f) =>
+      f.name.toLowerCase().includes(value.toLowerCase())
+    );
+    setData(result);
+  };
 
-const showSearchResultBar = () => {
-  setStyleSearchResult(prev => {
-    return {
-      ...prev,
-      display: "flex"
-    }
-  })
-}
-
-const hideSearchResultBar = () => {
-  setStyleSearchResult(prev => {
-    return {
-      ...prev,
-      display: "none"
-    }
-  })
-}
-
-const handleToolClick = (tool) => {
-  setSelectedTool(tool)
-}
+  const handleToolClick = (tool) => {
+    setSelectedTool(tool);
+  };
 
   return (
     <div className="searchTop">
       <div>
-        <input type="text" onBlur={hideSearchResultBar}  onKeyDown={showSearchResultBar} onChange={e => handleFilter(e.target.value)} placeholder="Search AI tool..." />
+        <input
+          type="text"
+          onChange={(e) => handleFilter(e.target.value)}
+          placeholder="Search AI tool..."
+        />
       </div>
-      <div className='searchResult' style={styleSearchResult}>
-        {data.map((d, i) => {
-              return (
-                <div className='seachResult' key={i} onClick={() => handleToolClick(d)}> {d.name} </div>
-              );
-            }
-          )
-        }
+      <div className="searchResult">
+        {data.map((d, i) => (
+          <div
+            className="searchItem"
+            key={i}
+            onClick={() => handleToolClick(d)}
+          >
+            {d.name}
+          </div>
+        ))}
       </div>
       <div className="displayComponent">
-        { selectedTool && (
+        {selectedTool && (
           <div>
-              <h2>{selectedTool.name}</h2>
-              <h2>{selectedTool.generate}</h2>
+            <h2>{selectedTool.name}</h2>
+            <h2>{  `${selectedTool.generate} generator`}</h2>
+            {/* Render additional properties and values here */}
           </div>
         )}
       </div>
     </div>
-    
   );
 };
 
